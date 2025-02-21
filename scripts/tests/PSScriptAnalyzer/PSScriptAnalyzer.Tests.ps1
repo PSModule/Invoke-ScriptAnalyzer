8efe415eede9f621b2dbafddcd75091f1facf0d8
Marius Storhaug <marstor@hotmail.com>
2025-02-21 22:17:39 +0100

    Test ANSI link in Github notice

--- scripts/tests/PSScriptAnalyzer/PSScriptAnalyzer.Tests.ps1
+++ scripts/tests/PSScriptAnalyzer/PSScriptAnalyzer.Tests.ps1
@@ -25,26 +25,29 @@ BeforeDiscovery {
         $rules = [Collections.Generic.List[System.Collections.Specialized.OrderedDictionary]]::new()
         $ruleObjects = Get-ScriptAnalyzerRule -Verbose:$false | Sort-Object -Property Severity, CommonName
         $Severeties = $ruleObjects | Select-Object -ExpandProperty Severity -Unique
+
+        Write-GitHubNotice -Message "`e]8;;https://example.com`e\Click here`e]8;;`e\"
+
         foreach ($ruleObject in $ruleObjects) {
-            $skip = if ($ruleObject.RuleName -in $settings.ExcludeRules) {
-                Write-Host " - $($ruleObject.RuleName)" -ForegroundColor DarkGray
-                Write-Host '   Skipping rule - Exclude list' -ForegroundColor DarkGray
-                $true
+            if ($ruleObject.RuleName -in $settings.ExcludeRules) {
+                Write-Host " - $($ruleObject.RuleName)"
+                Write-Host '   Skipping rule - Exclude list'
+                $skip = $true
             } elseif ($settings.IncludeRules -and $ruleObject.RuleName -notin $settings.IncludeRules) {
-                Write-Host " - $($ruleObject.RuleName)" -ForegroundColor DarkGray
-                Write-Host '   Skipping rule - Include list' -ForegroundColor DarkGray
-                $true
+                Write-Host " - $($ruleObject.RuleName)"
+                Write-Host '   Skipping rule - Include list'
+                $skip = $true
             } elseif ($settings.Severity -and $ruleObject.Severity -notin $settings.Severity) {
-                Write-Host " - $($ruleObject.RuleName)" -ForegroundColor DarkGray
-                Write-Host '   Skipping rule - Severity list' -ForegroundColor DarkGray
-                $true
+                Write-Host " - $($ruleObject.RuleName)"
+                Write-Host '   Skipping rule - Severity list'
+                $skip = $true
             } elseif ($settings.Rules -and $settings.Rules.ContainsKey($ruleObject.RuleName) -and -not $settings.Rules[$ruleObject.RuleName].Enable) {
-                Write-Host " - $($ruleObject.RuleName)" -ForegroundColor DarkGray
-                Write-Host '   Skipping rule  - Disabled' -ForegroundColor DarkGray
-                $true
+                Write-Host " - $($ruleObject.RuleName)"
+                Write-Host '   Skipping rule  - Disabled'
+                $skip = $true
             } else {
-                Write-Host " - $($ruleObject.RuleName)" -ForegroundColor Green
-                $false
+                Write-Host " - $($ruleObject.RuleName)"
+                $skip = $false
             }
 
             $rules.Add(
