e576b54872a8b207bca4ff2ccf9b16c99a3234a6
Marius Storhaug <marstor@hotmail.com>
2025-02-27 21:42:19 +0100

    🩹 [Patch]: Update PSScriptAnalyzer.Container.ps1 to use PSMODULE_INVOKE_PESTER_INPUT_Run_Path for Path variable

--- scripts/tests/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
+++ scripts/tests/PSScriptAnalyzer/PSScriptAnalyzer.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
     Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path             = $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Run_Path
+        Path             = $env:PSMODULE_INVOKE_PESTER_INPUT_Run_Path
         SettingsFilePath = $env:SettingsFilePath
         Debug            = $false
         Verbose          = $false
