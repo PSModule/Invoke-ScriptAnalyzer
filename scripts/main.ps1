b4712e1b3a29493d21061f8a9abe3273a3988b24
Marius Storhaug <marstor@hotmail.com>
2025-03-01 10:19:08 +0100

    🩹 [Patch]: Align `WorkingDirectory` usage in workflows and update pat… (#7)
    ## Description
    
    This pull request includes changes to ensure consistency in setting the
    `WorkingDirectory` parameter and improve the handling of paths in the
    script.
    
    ### Changes to workflows
    
    * `.github/workflows/Action-Test.yml`:
    * Consistently set the `WorkingDirectory` parameter for different job
    configurations to ensure the correct directory is used in each case.
    
    ### Documentation update
    
    * `README.md`:
    * Updated the `Path` parameter in the `Invoke PSScriptAnalyzer` job to
    use `src` instead of `${{ github.workspace }}` aligning with the change
    of adding a `WorkingDirectory`.
    
    ### Script improvement
    
    * `scripts/main.ps1`:
    * Improved the handling of the `Path` parameter by adding a check for an
    empty string and defaulting to `'.'` if empty, ensuring the script
    resolves the correct path.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,13 +1,14 @@
 ﻿# If test type is module, the code we ought to test is in the path/name folder, otherwise it's in the path folder.
 $settings = $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Settings
 $testPath = Resolve-Path -Path "$PSScriptRoot/tests/PSScriptAnalyzer" | Select-Object -ExpandProperty Path
-$codePath = Resolve-Path -Path $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path | Select-Object -ExpandProperty Path
+$path = [string]::IsNullOrEmpty($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path) ? '.' : $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path
+$codePath = Resolve-Path -Path $path | Select-Object -ExpandProperty Path
 $settingsFilePath = switch -Regex ($settings) {
     'Module|SourceCode' {
         "$testPath/$settings.Settings.psd1"
     }
     'Custom' {
-        Resolve-Path -Path "$env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath" | Select-Object -ExpandProperty Path
+        Resolve-Path -Path $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath | Select-Object -ExpandProperty Path
     }
     default {
         throw "Invalid test type: [$settings]"
