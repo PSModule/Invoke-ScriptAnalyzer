79492ca98837e63b1bf2cb25f07fa7c3e1860b75
Marius Storhaug <marstor@hotmail.com>
2025-10-11 21:40:53 +0200

    🩹 [Patch]: Refactor settings file path resolution logic for clarity and error handling

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -3,17 +3,15 @@ $testPath = Resolve-Path -Path "$PSScriptRoot/tests/PSScriptAnalyzer" | Select-O
 $path = [string]::IsNullOrEmpty($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path) ? '.' : $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path
 $codePath = Resolve-Path -Path $path | Select-Object -ExpandProperty Path
 
-# Try to resolve the settings file path, but allow it to be null if not found
-[string]$settingsFilePath = ''
-if (-not [string]::IsNullOrEmpty($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath)) {
-    try {
-        Write-Host "Looking for settings file under $($pwd.Path)"
-        $settingsFilePath = (Resolve-Path -Path $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath -ErrorAction Stop).Path
-        Write-Information "Using settings file: $settingsFilePath"
-    } catch {
-        Write-Warning "Settings file not found at path: $($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath). Using default settings."
-    }
+Write-Host "Looking for settings file under $($pwd.Path)"
+$tmpSettingsFilePath = Join-Path -Path $pwd.Path -ChildPath $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath
+$settingsFileExists = Test-Path -Path $tmpSettingsFilePath
+if ($settingsFileExists) {
+    $settingsFilePath = $tmpSettingsFilePath
+} else {
+    $settingsFilePath = ''
 }
+Write-Warning "Settings file not found at path: $($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath). Using default settings."
 
 [pscustomobject]@{
     CodePath         = $codePath
