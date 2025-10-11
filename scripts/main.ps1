1ffbf287413e92bcf6668704a7349d1d904a166a
Marius Storhaug <marstor@hotmail.com>
2025-10-11 17:10:00 +0200

    Reorder settings file validation to improve script flow

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -4,16 +4,16 @@ $path = [string]::IsNullOrEmpty($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path) 
 $codePath = Resolve-Path -Path $path | Select-Object -ExpandProperty Path
 $settingsFilePath = Join-Path -Path $codePath -ChildPath $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath
 
-if (!(Test-Path -Path $settingsFilePath)) {
-    Write-Error "Settings file not found at path: $settingsFilePath"
-    exit 1
-}
-
 [pscustomobject]@{
     CodePath         = $codePath
     TestPath         = $testPath
     SettingsFilePath = $settingsFilePath
 } | Format-List | Out-String
+
+if (!(Test-Path -Path $settingsFilePath)) {
+    Write-Error "Settings file not found at path: $settingsFilePath"
+    exit 1
+}
 
 Set-GitHubOutput -Name CodePath -Value $codePath
 Set-GitHubOutput -Name TestPath -Value $testPath
