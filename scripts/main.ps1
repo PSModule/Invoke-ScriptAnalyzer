14b3bb268e9cdbb0546aa778e111411c0fa584e9
Marius Storhaug <marstor@hotmail.com>
2025-10-11 17:22:29 +0200

    Refactor settings file path handling to use resolved path variable

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -2,18 +2,19 @@
 $testPath = Resolve-Path -Path "$PSScriptRoot/tests/PSScriptAnalyzer" | Select-Object -ExpandProperty Path
 $path = [string]::IsNullOrEmpty($env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path) ? '.' : $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_Path
 $codePath = Resolve-Path -Path $path | Select-Object -ExpandProperty Path
+$settingsFilePath = Resolve-Path -Path $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath | Select-Object -ExpandProperty Path
 
 [pscustomobject]@{
     CodePath         = $codePath
     TestPath         = $testPath
-    SettingsFilePath = $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath
+    SettingsFilePath = $settingsFilePath
 } | Format-List | Out-String
 
-if (!(Test-Path -Path $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath)) {
-    Write-Error "Settings file not found at path: $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath"
+if (!(Test-Path -Path $settingsFilePath)) {
+    Write-Error "Settings file not found at path: $settingsFilePath"
     exit 1
 }
 
 Set-GitHubOutput -Name CodePath -Value $codePath
 Set-GitHubOutput -Name TestPath -Value $testPath
-Set-GitHubOutput -Name SettingsFilePath -Value $env:PSMODULE_INVOKE_SCRIPTANALYZER_INPUT_SettingsFilePath
+Set-GitHubOutput -Name SettingsFilePath -Value $settingsFilePath
